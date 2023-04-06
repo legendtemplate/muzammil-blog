dbf3e7337c00aaa8f1adbad4c4f00b0f5c874cc7
Muhammad Muzammil Safdar <129264399+legendtemplate@users.noreply.github.com>
2023-04-06 21:55:40 +0500

    Update index.tsx

--- components/about/team/index.tsx
+++ components/about/team/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 export default function index() {
   return (
@@ -9,16 +8,16 @@ export default function index() {
           <div className="col-md-8 col-lg-6">
             <div className="header-section mb-12">
               <h3 className=" mb-6 text-base font-medium text-red-500">Our Experts</h3>
-              <h2 className="font-bold text-5xl">Let meet with our team members</h2>
+              <h2 className="font-bold text-5xl">Let's meet with our team members</h2>
             </div>
           </div>
         </div>
         <div className="row flex flex-wrap justify-center items-center">
           {secondcolumn.map((navItem) => (
-            <div className="lg:w-3/12 md:w-6/12 w-ful px-3" {navItem.id}>
+            <div className="lg:w-3/12 md:w-6/12 w-ful px-3">
               <div className="single-person mt-7 p-7 bg-slate-100 rounded">
                 <div className="person-image relative mb-12 rounded-full">
-                  <Image
+                  <img
                   className="w-full rounded-full"
                     src={navItem.img}
                     alt={navItem.name}
@@ -42,7 +41,7 @@ export default function index() {
 }
 
 interface NavItem {
-  id:number,
+  id: number;
   speciality: string;
   name: string;
   img: string;
@@ -50,22 +49,25 @@ interface NavItem {
 
 const secondcolumn: Array<NavItem> = [
   {
-    id:1,
+    id: 1,
     speciality: "Web Developer ",
     name: "Muhammad Muzammil Safdar",
     img: "https://cdn-icons-png.flaticon.com/512/1946/1946429.png",
   },
-  {    id:2,
+  {
+    id: 2,
     speciality: "Graphic Designer",
     name: "Muhammad Rashid Ali",
     img: "https://cdn-icons-png.flaticon.com/512/1946/1946429.png",
   },
-  {    id:3,
+  {
+    id: 3,
     speciality: "Data Entry",
     name: "Muhammad Imran Safdar",
     img: "https://cdn-icons-png.flaticon.com/512/1946/1946429.png",
   },
-  {    id:4,
+  {
+    id: 4,
     speciality: "Vedio Editing",
     name: "Muhammad Saim Saeed",
     img: "https://cdn-icons-png.flaticon.com/512/1946/1946429.png",
